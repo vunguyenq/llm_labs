a3f9ee4a2157082900dac00a38e0fe59965f04d0
vunguyenq <vu.nguyenq89@gmail.com>
2025-05-05 00:52:37 +0200

    create FastAPI for resume_parser

---
+++ resume_parser/endpoints/ep_parse_resume.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from resume_parser.parser import ResumeChecklist, parse_resume
+
+# Create a router for this endpoint
+router = APIRouter()
+
+# Define the GET endpoint
+@router.get("/parse_resume", response_model=ResumeChecklist)
+async def parse_resume_endpoint(resume_text: str):
+    parsed_data = parse_resume(resume_text)
+    return parsed_data
